625824f46cd3c37fdbc57cc47b31077e8b2c2222
denizzqq <denizdolanmaz@rocketmail.com>
2026-01-29 20:08:20 +0100

    dummy

--- src/main/python/systemds/context/systemds_context.py
+++ src/main/python/systemds/context/systemds_context.py
@@ -299,7 +299,8 @@ class SystemDSContext(object):
         :param port: The port address to use if -1 chose random port."""
 
         # Base command
-        command = ["java", "--add-modules=jdk.incubator.vector", "-cp"]
+        # Note: locale flags added to avoid Turkish locale issues with case conversion
+        command = ["java", "-Duser.language=en", "-Duser.country=US", "--add-modules=jdk.incubator.vector", "-cp"]
 
         # Find the operating system specifc separator, nt means its Windows
         cp_separator = ";" if os.name == "nt" else ":"
